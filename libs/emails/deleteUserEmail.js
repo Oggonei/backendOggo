27af2005e00f2b4a6ff097cc74ff6e1e4a0e24f2
Cristhian Santos <oggonei@hotmail.com>
2024-09-09 16:09:43 -0500

    Delete User email added

--- libs/emails/deleteUserEmail.js
+++ libs/emails/deleteUserEmail.js
@@ -4,9 +4,9 @@ const deleteUserEmail = async (token,email) => {
     const info = await transporter.sendMail({
         from: 'notificaciones@oggo.com.co', // sender address
         to: email, // list of receivers
-        subject: "Verify OGGO Acount", // Subject line
-        text: "Verify OGGO Acount", // plain text body
-        html: `<b>Para verificar tu cuenta ingresa al <a href=frontURL/${token}>siguiente link</a></b>` // html body
+        subject: "Delete OGGO Acount", // Subject line
+        text: "Delete OGGO Acount", // plain text body
+        html: `<p>Su cuenta ha sido eliminada satisfactoriamente</p>` // html body
     })
     console.log('Mensaje enviado: ',info.messageId)
     return info
